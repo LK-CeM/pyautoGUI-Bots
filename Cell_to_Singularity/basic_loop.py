540980d62be351db8fd7b00ec255feddc989a3f3
Gleichlucas <lucas.kunz94@gmail.com>
2022-01-20 19:50:02 +0100

    working dumb bot

--- Cell_to_Singularity/basic_loop.py
+++ Cell_to_Singularity/basic_loop.py
@@ -1,19 +1,25 @@
 import pyautogui as pag
 import time
 import sys
+import os
 
 
 def setup():
     pag.FAILSAFE = True
     screenWidth, screenHeight = pag.size()
-    time.sleep(5)
+    time.sleep(2)
     print('Setup complete')
 
 def main(argv):
     setup()
     while (True):
         pag.write("hello world")
-        time.sleep(1)
+        pag.screenshot('test.png')
+        for dinoPic in os.listdir("./dinoPictures/"):
+            if dinoPic.endswith(".png"):
+                dinoPos = (pag.locateOnScreen("./dinoPictures/"+dinoPic, confidence=0.9))
+                pag.click(dinoPos[0],dinoPos[1])
+        time.sleep(2)
 
 
 if __name__ == "__main__":
