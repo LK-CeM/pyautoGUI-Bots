6440374deaef90e48d845bb6fc857e19934679cc
LK-CeM <lucas.kunz94@gmail.com>
2023-02-03 19:15:42 +0000

    added repair macro

--- tarkov/mouse_test.py
+++ tarkov/mouse_test.py
@@ -8,14 +8,36 @@ import win32gui, win32ui, win32con, win32api
 
 def main(argv):
     while(True):
-        if (keyboard.is_pressed('p')):
+        if (keyboard.is_pressed('p')and keyboard.is_pressed('alt')):
             playback("junkbox_top_left.txt")
-        if (keyboard.is_pressed('r')):
+        if (keyboard.is_pressed('r') and keyboard.is_pressed('alt')):
             record_mouse()
-        if (keyboard.is_pressed('e')):
+        elif (keyboard.is_pressed('f')):
             exit()
+        elif (keyboard.is_pressed('e')):
+            repair()
+
+def change_pos(x, y):
+    pos = list(pag.position())
+    pos[0] += x
+    pos[1] += y
+    pos = tuple(pos)
+    return pos
+
+def repair(): #1432 101, 1481 230, 981 906 
+    print("starting repair...")
+    org_pos = pag.position()
+    pag.click(button='right')
+    pag.moveTo(pag.locateOnScreen('./imgs/repair.png', confidence=0.8))
+    pag.click()
+    pag.moveTo((981,  906))
+    pag.click()
+    time.sleep(0.15)
+    pag.press("esc")
+    pag.moveTo(org_pos)
 
 def playback(file):
+    print("starting playback...")
     f = open(file, "r")
     lines = f.readlines()
     test = [[int(j) for j in x.split()] for x in lines]
@@ -30,7 +52,9 @@ def playback(file):
 
 
 
-def record_mouse():
+
+def record_mouse(): #press a to record pos
+    print("start recording...")
     file = "pos.txt"
     f = open(file, 'a')
     while(True):
